8ac28e2dde29b4f1a27dbef55f24251ce655a465
Ajay Khatri <amajaying@gmail.com>
2022-01-01 14:39:10 +0545

    Fixed Back Button Touch Issue

--- app/src/main/java/com/example/kiitappwithinstaclone/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/kiitappwithinstaclone/AccountSettingsActivity.kt
@@ -3,6 +3,12 @@ package com.example.kiitappwithinstaclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kiitappwithinstaclone.databinding.ActivityAccountSettingsBinding
+import android.content.Intent
+
+import android.net.Uri
+
+
+
 
 
 class AccountSettingsActivity : AppCompatActivity() {
@@ -13,5 +19,16 @@ class AccountSettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAccountSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.backButton.setOnClickListener{
+            finish()
+        }
+
+        binding.gotoPortal.setOnClickListener{
+            val uri = Uri.parse("https://kiitportal.kiituniversity.net/irj/portal/")
+
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/kiitappwithinstaclone/LoginActivity.kt
+++ app/src/main/java/com/example/kiitappwithinstaclone/LoginActivity.kt
@@ -9,6 +9,13 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener{
+
+        }
     }
 }
