2ce3234c0bdd962971ea0dcb9656042a26fca5fd
Ajay Khatri <amajaying@gmail.com>
2022-01-01 22:55:00 +0545

    Fixed "Showing login screen evenif the user is logged in" issue!

--- app/src/main/java/com/example/kiitappwithinstaclone/LoginActivity.kt
+++ app/src/main/java/com/example/kiitappwithinstaclone/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.kiitappwithinstaclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kiitappwithinstaclone.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,5 +20,17 @@ class LoginActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener{
 
         }
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        if(FirebaseAuth.getInstance().currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
