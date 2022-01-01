11d4f66413a6f58d3a761e33cc057dd8ce005b7d
Ajay Khatri <amajaying@gmail.com>
2022-01-01 19:43:48 +0545

    Added User Adding function just for admin

--- app/src/main/java/com/example/kiitappwithinstaclone/SignupActivity.kt
+++ app/src/main/java/com/example/kiitappwithinstaclone/SignupActivity.kt
@@ -1,32 +1,29 @@
 package com.example.kiitappwithinstaclone
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.widget.Toast
-import com.bumptech.glide.util.Util
-import com.example.kiitappwithinstaclone.databinding.ActivitySignUpBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kiitappwithinstaclone.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlin.collections.HashMap as HashMap
 
-class SignUP : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var binding:ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
         binding.save.setOnClickListener {
-            CreateAccount()
+            createAccount()
         }
     }
 
-    private fun CreateAccount() {
+    private fun createAccount() {
         val fullname = binding.fullName.text.toString()
         val email = binding.emaill.text.toString()
         val password = binding.pass.text.toString()
@@ -41,29 +38,29 @@ class SignUP : AppCompatActivity() {
         mAuth.createUserWithEmailAndPassword(email,password)
             .addOnCompleteListener(this) { task ->
                 if(task.isSuccessful){
-                    SaveuserInfo(fullname,email,phoneNo,rollNo,address,school,semester)
+                    saveuserInfo(fullname,email,phoneNo,rollNo,address,school,semester)
                 }
                 else{
                     val message = task.exception!!.toString()
                 }
 
 
+            }
     }
-}
 
-    private fun SaveuserInfo(fullname: String, email: String, phoneNo: String, rollNo: String, address: String, school: String, semester: String) {
+    private fun saveuserInfo(fullname: String, email: String, phoneNo: String, rollNo: String, address: String, school: String, semester: String) {
         val currentUserId = FirebaseAuth.getInstance().currentUser!!.uid
         val usersRef: DatabaseReference = FirebaseDatabase.getInstance().reference.child("Users")
 
         val userMap = HashMap<String, Any>()
         userMap["uid"] = currentUserId
-        userMap["fullname"] = currentUserId
-        userMap["email"] = currentUserId
-        userMap["phoneNo"] = currentUserId
-        userMap["rollNo"] = currentUserId
-        userMap["address"] = currentUserId
-        userMap["school"] = currentUserId
-        userMap["semester"] = currentUserId
+        userMap["fullname"] = "Ajay Khatri Chhetri"
+        userMap["email"] = "amajaying@gmail.com"
+        userMap["phoneNo"] = "+9779867234454"
+        userMap["rollNo"] = "21053264"
+        userMap["address"] = "Tilottama, Nepal"
+        userMap["school"] = "CSE"
+        userMap["semester"] = "First Semester"
         userMap["image"] = "gs://appfinalkiit.appspot.com/Default Images/default_profile.jpg"
 
         usersRef.child(currentUserId).setValue(userMap)
@@ -72,5 +69,5 @@ class SignUP : AppCompatActivity() {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
         finish()
-            }
     }
+}
