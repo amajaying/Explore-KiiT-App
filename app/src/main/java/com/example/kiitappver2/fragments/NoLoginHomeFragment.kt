ad444d97abf2c22522fdaecb0cd450d6ea5f4ba0
Ajay Khatri <amajaying@gmail.com>
2022-01-04 14:43:44 +0545

    Added want tour? function

--- app/src/main/java/com/example/kiitappver2/fragments/NoLoginHomeFragment.kt
+++ app/src/main/java/com/example/kiitappver2/fragments/NoLoginHomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.kiitappver2.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -50,16 +52,29 @@ class NoLoginHomeFragment : Fragment() {
         _binding = FragmentNoLoginHomeBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
 
+
+        //Image Slder Start
         val imageSlider = binding.imageSlier
         val imageList = ArrayList<SlideModel>()
+
 
         imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/appfinalkiit.appspot.com/o/SliderImages%2Fslider_four.png?alt=media&token=4bcf1fb3-717f-4d77-b902-c57ab24798b8"))
         imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/appfinalkiit.appspot.com/o/SliderImages%2Fslider_one.png?alt=media&token=3431cc5f-eece-45e0-9619-63c45eefc024"))
         imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/appfinalkiit.appspot.com/o/SliderImages%2Fslider_three.png?alt=media&token=b913792e-af40-46ba-bd11-cd7844bf48d4"))
         imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/appfinalkiit.appspot.com/o/SliderImages%2Fslider_two.png?alt=media&token=7e26353d-ab40-425b-a988-b6c9353445ac"))
 
-
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
+        //Image Slider Finish
+
+
+        //Virtual Tour Button
+        binding.virtualToutbtn.setOnClickListener{
+            val uri = Uri.parse("https://kiit.ac.in/tour/")
+
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+        //Virtual Button Finish
 
         return binding.root
     }
