5826e183db8111909c960ac89667fc597b1b4911
Ajay Khatri <amajaying@gmail.com>
2022-01-05 17:19:42 +0545

    Added Founder page

---
+++ app/src/main/java/com/example/kiitappver2/FounderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kiitappver2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FounderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_founder)
+    }
+}
