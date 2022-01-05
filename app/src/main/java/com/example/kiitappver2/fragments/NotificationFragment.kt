996b97b9334296cd6b5761bbee9c8ba8ae13c992
Ajay Khatri <amajaying@gmail.com>
2022-01-05 10:44:42 +0545

    added Notification Show Page

--- app/src/main/java/com/example/kiitappver2/fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/kiitappver2/fragments/NotificationFragment.kt
@@ -5,11 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kiitappver2.Notification
+import com.example.kiitappver2.NotificationAdapter
 import com.example.kiitappver2.R
-import com.example.kiitappver2.Society
-import com.example.kiitappver2.databinding.FragmentNoLoginHomeBinding
 import com.example.kiitappver2.databinding.FragmentNotification2Binding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,9 +29,10 @@ class NotificationFragment : Fragment() {
 
     private lateinit var notiRecyclerView: RecyclerView
     private lateinit var notiArrayList: ArrayList<Notification>
-    lateinit var notiTitle: Array<String>
-    lateinit var notiDetail: Array<String>
-    lateinit var notiImage : Array<Int>
+
+    private lateinit var notiTitle: Array<String>
+    private lateinit var notiDetail: Array<String>
+    private lateinit var notiImage : Array<Int>
 
     private var _binding: FragmentNotification2Binding? = null
 
@@ -54,9 +55,39 @@ class NotificationFragment : Fragment() {
         _binding = FragmentNotification2Binding.inflate(inflater, container, false)
 
 
+        notiTitle = arrayOf(
+            "Winter Vacation",
+            "Winter Vacation"
+        )
 
 
+        notiDetail = arrayOf(
+            "It is to be notified that winter vacation from Dec 21 to Jan2 is provided to the students.",
+            "It is to be notified that winter vacation from Dec 21 to Jan2 is provided to the students."
+        )
+
+        notiImage = arrayOf(
+            R.drawable.img_society_one,
+            R.drawable.img_society_one
+        )
+
+
+        notiRecyclerView = binding.recyclerViewNotification
+        notiRecyclerView.layoutManager = LinearLayoutManager(context)
+        notiRecyclerView.setHasFixedSize(true)
+
+        notiArrayList = arrayListOf<Notification>()
+        getUserData()
         return binding.root
+    }
+
+    private fun getUserData() {
+        for(i in notiImage.indices){
+            val notification = Notification(notiTitle[i], notiDetail[i], notiImage[i])
+            notiArrayList.add(notification)
+        }
+        val notificationAdapter = NotificationAdapter(notiArrayList)
+        notiRecyclerView.adapter = notificationAdapter
     }
 
     companion object {
