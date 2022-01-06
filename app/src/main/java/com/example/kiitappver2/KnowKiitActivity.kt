a23974c92ee654c52c79716b6d78ba3d067f0512
Ajay Khatri <amajaying@gmail.com>
2022-01-06 13:34:17 +0545

    added Know Kiit Page

---
+++ app/src/main/java/com/example/kiitappver2/KnowKiitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kiitappver2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KnowKiitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_know_kiit)
+    }
+}
