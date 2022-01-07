cb36c49517957f4dc9709881926e621a7d26f67e
Ajay Khatri <amajaying@gmail.com>
2022-01-07 10:14:08 +0545

    adding My Notes Function

--- app/src/main/java/com/example/kiitappver2/NotesActivity.kt
+++ app/src/main/java/com/example/kiitappver2/NotesActivity.kt
@@ -2,10 +2,23 @@ package com.example.kiitappver2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kiitappver2.databinding.ActivityNotesBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NotesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNotesBinding
+
+    lateinit var notesRV: RecyclerView
+    lateinit var addbtn:FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notes)
+        binding = ActivityNotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        notesRV = binding.notesRV
+        addbtn = binding.addNotebtn
     }
 }
