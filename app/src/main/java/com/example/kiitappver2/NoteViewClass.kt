968c3dd2737a1956c5d5e66891cbc62f6706eaab
Ajay Khatri <amajaying@gmail.com>
2022-01-07 09:47:40 +0545

    adding My Notes Function

--- app/src/main/java/com/example/kiitappver2/NoteViewClass.kt
+++ app/src/main/java/com/example/kiitappver2/NoteViewClass.kt
@@ -1,4 +1,35 @@
 package com.example.kiitappver2
 
-class NoteViewClass {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
+
+@InternalCoroutinesApi
+class NoteViewClass(application: Application): AndroidViewModel(application) {
+
+    private val allNotes: LiveData<List<Note>>
+    private val repository: NoteRepository
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNotesDao()
+        repository = NoteRepository(dao)
+        allNotes = repository.allNotes
+    }
+
+    fun deleteNote(note:Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.delete(note)
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.update(note)
+    }
+
+    fun addNote(note: Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.insert(note)
+    }
+
 }
