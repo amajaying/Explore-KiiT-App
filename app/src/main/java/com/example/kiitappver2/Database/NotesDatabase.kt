107a052e37cea8f1e86e7f3c2426a42f63b253bd
Ajay Khatri <amajaying@gmail.com>
2022-01-08 01:04:27 +0545

    adding Notes Section

--- app/src/main/java/com/example/kiitappver2/Database/NotesDatabase.kt
+++ app/src/main/java/com/example/kiitappver2/Database/NotesDatabase.kt
@@ -1,10 +1,13 @@
 package com.example.kiitappver2.Database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.kiitappver2.Dao.NotesDao
+import com.example.kiitappver2.Model.Notes
 
+@Database(entities = [Notes::class], version = 1, exportSchema = false)
 abstract class NotesDatabase : RoomDatabase() {
     abstract fun myNotesDao(): NotesDao
 
--- app/src/main/java/com/example/kiitappver2/notes/NoteFragment/CreateNoteFragment.kt
+++ app/src/main/java/com/example/kiitappver2/notes/NoteFragment/CreateNoteFragment.kt
@@ -7,15 +7,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.example.kiitappver2.Model.Notes
 import com.example.kiitappver2.R
+import com.example.kiitappver2.ViewModel.NotesViewModel
 import com.example.kiitappver2.databinding.FragmentCreateNoteBinding
 import com.google.gson.internal.bind.util.ISO8601Utils.format
+import kotlinx.android.synthetic.main.soc_row_category.*
 import java.util.*
 
 
 class CreateNoteFragment : Fragment() {
 
-    private lateinit var binding:FragmentCreateNoteBinding
+    private lateinit var binding: FragmentCreateNoteBinding
+    val viewmodel: NotesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,10 +38,20 @@ class CreateNoteFragment : Fragment() {
 
     private fun createNotes(it: View?) {
 
-        val title = binding.noteTitle.text
-        val description = binding.noteDescription.text
+        val title = binding.noteTitle.text.toString()
+        val description = binding.noteDescription.text.toString()
         val d = Date()
-        val  notesDate : CharSequence = DateFormat.format("MMMM d, yyyy ", d.getTime())
+        val notesDate: CharSequence = DateFormat.format("MMMM d, yyyy ", d.getTime())
+
+        val data = Notes(
+            null, title = title,
+            notes = description,
+            date=notesDate.toString()
+        )
+        viewmodel.addNotes(data)
+
+        Toast.makeText(context,"Note Created Successfully!", Toast.LENGTH_SHORT).show()
+
 
 
     }
